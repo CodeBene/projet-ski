def3877f75d07cacff972e997f519dd23321d9ed
Shweeby <stephanie.hamelin@gmail.com>
2022-03-18 20:02:00 -0400

    Ajout du localstorage et du redirect sur le profil

--- main.js
+++ main.js
@@ -7,22 +7,42 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./localstorage');
+}
 
 app.get("/signup", (request, response) =>{
-    response.render("signup")
-})
+    response.render("signup");
+});
 
 app.get("/signin", (request, response) =>{
-    response.render("signin")
-})
+    response.render("signin");
+});
 
 app.get("/", (request, response) =>{
-    response.redirect("signin")
-})
+    response.redirect("signin");
+});
+
+app.get("/profil", (request, response) =>{
+    let token = localStorage.getItem("token");
+    var config = {
+        method: 'get',
+        url: 'https://ski-api.herokuapp.com/tokeninfo',
+        headers: {'Content-Type': 'application/json', 'Authorization': token},
+        data : {'token': token}
+    };
+    axios(config)
+    .then(function (res) {
+        response.render("profil", res.data);
+    })
+    .catch(function (error) {
+        console.log(token);
+        response.send("Désolé, vous n'avez pas un compte, merci de vous s'inscrire");
+    });
+});
 
 app.post("/signin", (request, response)=>{
-    console.log(request);
     const email = request.body.email;
     const password = request.body.password;
 
@@ -36,12 +56,13 @@ app.post("/signin", (request, response)=>{
 
 axios(config)
 .then(function (res) {
-    response.render("profil", res.data)
+    localStorage.setItem("token", res.data.token);
+    response.redirect("/profil");
 })
 .catch(function (error) {
   response.send("Désolé, vous n'avez pas un compte, merci de vous s'inscrire");
 });
-})
+});
 
 app.post("/signup", (request, response)=>{
     const name = request.body.name;
@@ -58,13 +79,13 @@ app.post("/signup", (request, response)=>{
     };
     axios(config)
     .then(function (res) {
-    response.render("profil", res.data)
-})
+        response.render("profil", res.data);
+    })
     .catch(function (error) {
-    console.log(error);
+        console.log(error);
+    });
 });
-})
 
 app.listen(3000, ()=>{
-    console.log("Votre serveur demarre par la porte 3000")
-})
+    console.log("Votre serveur demarre par la porte 3000");
+});
